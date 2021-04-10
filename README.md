22a863b90cb67ab5dcf3baaa2a12da133bc6c638
Glitch (cookie-explorer) <none>
2021-04-10 22:13:01 +0000

    Adding skeleton from Glitch

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# cookie-explorer
+# Cookie explorer
+
+A simple skeleton web app using Express and Node to experiment with cookies.
+
+---
+
+### Built with [Glitch](https://glitch.com/)
---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const app = express();
+
+const TheDefaultList = [
+  "Earth",
+  "Wind",
+  "Fire"
+];
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// sample endpoint
+app.get("/thelist", (request, response) => {
+  // express helps us take JS objects and send them as JSON
+  response.json(TheDefaultList);
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
